513abd4cf2c0c999971f177b4e6aedf183132188
krunuchkrucom-web <krunuch.krucom@gmail.com>
2026-04-17 10:45:03 +0700

    student.js

--- student.js
+++ student.js
@@ -17,7 +17,7 @@ window.onload = () => {
 };
 
 async function fetchStudentData() {
-    const API_URL = "https://script.google.com/macros/s/AKfycbwHV8K0Me5It_ePtkt4EhEnFPzypA6Rdpl-zmpU-vABr2fTeFYQGI8DZSppXSggnuPbtw/exec";
+    const API_URL = "https://script.google.com/macros/s/AKfycbyPhlmzhDEL8ZK45r-ntJz-FrrKdJs7enX55caorz00yYI4Tkz47tX0VMnBIZdg_j6TZQ/exec";
     
     try {
         // ส่ง Action ไปที่ Google Apps Script เพื่อขอข้อมูลรายคน
