cfb14bf8f94a528a2b9f74946e26df5949a8b1d6
krunuchkrucom-web <krunuch.krucom@gmail.com>
2026-04-22 15:23:32 +0700

    scanner.js

--- scanner.js
+++ scanner.js
@@ -1,1 +1,17 @@
-
+// ฟังก์ชันบันทึกข้อมูลลง Google Sheet
+function saveToSheet(studentData) {
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbx-_wojFp6rkj_FRyfWo2K1eQvjyiLKeEQbjDTolRcvPzInyC6q5CmWpE0M2cC-pMPIhQ/exec';
+    
+    fetch(scriptURL, {
+        method: 'POST',
+        body: JSON.stringify(studentData)
+    })
+    .then(response => {
+        Swal.fire('สำเร็จ!', 'บันทึกข้อมูลลงฐานข้อมูลแล้ว', 'success');
+        updateScanHistory(studentData); // อัปเดตประวัติหน้าเว็บ
+    })
+    .catch(error => {
+        console.error('Error!', error.message);
+        Swal.fire('ผิดพลาด', 'ไม่สามารถบันทึกข้อมูลได้', 'error');
+    });
+}
